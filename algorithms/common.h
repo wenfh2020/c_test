5626035a26cd4c12a0e72f42d15cdfdb361b69cb
wenfh2020 <wenfh2020@126.com>
2019-12-10 05:39:37 +0800

    整理文件，增加注释

--- algorithms/common.h
+++ algorithms/common.h
@@ -7,42 +7,13 @@
 #include <iostream>
 using namespace std;
 
-#define _LOG
-const int g_buf_size = 1024 * 1024;
+// #define _LOG
+#define BUF_SZIE (1024 * 1024)
 
-int log(const char* args, ...) {
-    int ret = 0;
-
-#ifdef _LOG
-    va_list ap;
-    char buf[g_buf_size] = {0};
-
-    va_start(ap, args);
-    ret = vsprintf(buf, args, ap);
-    printf("%s", buf);
-    va_end(ap);
-#endif
-
-    return ret;
-}
-
-void print_array(int array[], int start, int end, const char* str = "") {
-    strlen(str) == 0 ? log("size:%d --> ", end - start + 1)
-                     : log("%s, size:%d --> ", str, end - start + 1);
-    for (int i = start; i <= end; i++) {
-        log("%d, ", array[i]);
-    }
-    log("\n");
-}
-
-bool check(int array[], int len) {
-    for (int i = 0; i < len - 1; i++) {
-        if (array[i] > array[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
+void swap(int* a, int* b);
+int log(const char* args, ...);
+bool check(int array[], int len);
+void print_array(int array[], int start, int end, const char* str = "");
 
 class Cost {
    public:
@@ -56,11 +27,5 @@ class Cost {
    private:
     clock_t m_begin_time;
 };
-
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
 #endif  //_COMMAND_H_
