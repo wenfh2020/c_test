7c1aa53892843b17f885bd2ab935c2688961c410
wenfh2020 <wenfh2020@126.com>
2020-01-02 09:32:39 +0800

    测试进程 fork 以及僵尸进程

--- normal/proc/main.cpp
+++ normal/proc/main.cpp
@@ -1,4 +1,8 @@
+// fork 测试 copy-on-write
+// wenfahua/2020-01-02
+
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,70 +10,115 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <iostream>
-/*
-《深入理解计算机系统》 第二部分 - 8.4 异常流控制
-fork 函数会返回两次，第一次在父进程中返回， pid > 0， 第二次在子进程中返回 pid
-== 0
-fork 子进程会拷贝父进程数据，可以从变量的变化中理解逻辑。
-*/
 
-void test_fork() {
-    int iTest = 0;
-    printf("begin...\n");
+#define MEM_SIZE (1024 * 1024 * 1024 / 4)
 
-    for (int i = 0; i < 2; i++) {
-        pid_t iPid = fork();  // fork return twice, so..
-        if (0 == iPid) {
-            printf("pid: %d, child, test num: %d\n", getpid(), --iTest);
-            return;
-        } else if (iPid > 0) {
-            printf("pid = %d, parent, test num: %d\n", getpid(), ++iTest);
-        } else {
-            printf("failed!\n");
-            return;
+// 修改测试数据
+void format_data(int* array, size_t len) {
+    if (array) {
+        srand((unsigned)time(NULL));
+        for (size_t i = 0; i < len; i++) {
+            array[i] = i + rand() % len;
         }
-
-        // printf("pid = %d, test index %d\n", getpid(), i);
     }
-
-    printf("pid: %d, end..., test num: %d\n", getpid(), iTest);
 }
 
-#define N 2
-
-void test_waitpid() {
-    int status, i;
-    pid_t pid[N], retpid;
-
-    for (i = 0; i < N; i++) {
-        if ((pid[i] = fork()) == 0) {
-            printf("exit from child pid: %d\n", getpid());
-            exit(100 + i);
+// 测试内存对象
+class alloc_data {
+   public:
+    alloc_data() : m_array(NULL), m_len(MEM_SIZE) {
+        m_array = new int[m_len];
+        if (m_array != NULL) {
+            format_data(m_array, m_len);
+            printf("alloc array, this: %lx, ptr: %lx\n", (unsigned long)this,
+                   (unsigned long)&m_array);
+        } else {
+            printf("alloc data fail, pid: %d\n", getpid());
         }
     }
 
-    i = 0;
+    virtual ~alloc_data() {
+        if (m_array != NULL) {
+            delete[] m_array;
+            printf("delete data, pid: %d\n", getpid());
+        }
+    }
 
-    while ((retpid = waitpid(-1, &status, 0)) > 0) {
+    void reset() {
+        printf("reset array, this: %#lx, ptr: %#lx\n", (unsigned long)this,
+               (unsigned long)&m_array);
+        format_data(m_array, m_len);
+    }
+    int get_data_len() { return m_len; }
+
+   private:
+    int* m_array;
+    int m_len;
+};
+
+alloc_data g_data;
+
+void child_handler(int signal_num) {
+    pid_t pid;
+    int status;
+    int old_errno = errno;
+
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
         if (WIFEXITED(status)) {
-            printf("child %d terminated normally with exit status = %d\n",
-                   retpid, WEXITSTATUS(status));
+            printf("child %d terminated normally with exit status = %d\n", pid,
+                   WEXITSTATUS(status));
         } else {
-            printf("child %d terminated abnormally\n", retpid);
+            printf("child %d terminated abnormally\n", pid);
         }
     }
 
     if (errno != ECHILD) {
         printf("waitpid error\n");
-        exit(1);
+        return;
     }
 
-    printf("exit end\n");
-    exit(0);
+    errno = old_errno;
+    printf("errno: %d, exit end\n", errno);
+}
+
+void do_some_thing() {
+    int i = 0;
+    for (int k = 0; k < 100000000; k++) {
+        i = i + 2;
+        if (k % 1000 == 0) {
+            usleep(100);
+        }
+    }
+}
+
+void signal_set() {
+    struct sigaction sigchild_action;
+    memset(&sigchild_action, 0, sizeof(sigchild_action));
+    sigchild_action.sa_handler = &child_handler;
+    sigaction(SIGCHLD, &sigchild_action, NULL);
 }
 
 int main() {
-    test_fork();
-    // test_waitpid();
+    signal_set();
+    pid_t pid = fork();
+    if (0 == pid) {
+        printf("child pid: %d\n", getpid());
+        sleep(10);  // update data before
+        g_data.reset();
+        sleep(10);  // update data later
+        exit(0);
+    } else if (pid > 0) {
+        printf("parent pid: %d\n", getpid());
+        // sleep(10);
+        // sleep() makes the calling process sleep until seconds seconds have
+        // elapsed or a signal arrives which is not ignored.
+    } else {
+        // fail
+        printf("fail\n");
+        exit(1);
+    }
+
+    do_some_thing();
+    printf("main end2, pid: %d\n", getpid());
     return 0;
 }
--- normal/proc/zombie.cpp
+++ normal/proc/zombie.cpp
@@ -1,120 +1,26 @@
-#include <errno.h>
-#include <signal.h>
+// fork 测试僵尸进程
+// wenfahua/2020-01-02
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <iostream>
 
-// 测试 fork，处理僵尸进程，添加 SIGCHLD 信号处理僵尸进程。
-
-#define CHILD_NUM 4
-#define MEM_SIZE (1024 * 1024 / 4)
-
-// 修改测试数据
-void format(int* array, size_t len) {
-    srand((unsigned)time(NULL));
-    for (size_t i = 0; i < len; i++) {
-        array[i] = i + rand() % len;
-    }
-}
-
-// 测试内存对象
-class alloc_data {
-   public:
-    alloc_data() : m_array(NULL), m_len(0) {
-        int* m_array = new int[MEM_SIZE];
-        if (m_array != NULL) {
-            m_len = MEM_SIZE;
-            format(m_array, MEM_SIZE);
-        } else {
-            printf("alloc data fail, pid: %d\n", getpid());
-        }
-    }
-
-    virtual ~alloc_data() {
-        if (m_array != NULL) delete[] m_array;
-        printf("delete data, pid: %d\n", getpid());
-    }
-
-    int get_data_len() { return m_len; };
-    void reset() { format(m_array, m_len); }
-
-   private:
-    int* m_array;
-    int m_len;
-};
-
-alloc_data data;
-
-void do_some_thing() {
-    int i = 0;
-    while (1) {
-        for (int k = 0; k < 1000; k++) {
-            i = i * 2;
-            if (k % 800 == 0) {
-                usleep(100);
-            }
-        }
-    }
-}
-
-// sigchld 信号处理
-void child_handler(int signal_num) {
-    pid_t pid;
-    int status;
-    int old_errno = errno;
-
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        if (WIFEXITED(status)) {
-            printf("child %d terminated normally with exit status = %d\n", pid,
-                   WEXITSTATUS(status));
-        } else {
-            printf("child %d terminated abnormally\n", pid);
-        }
-    }
-
-    if (errno != ECHILD) {
-        printf("waitpid error\n");
-        return;
-    }
-
-    errno = old_errno;
-    printf("errno: %d, exit end\n", errno);
-}
-
-// 信号关注设置
-void signal_set() {
-    struct sigaction sigchild_action;
-    memset(&sigchild_action, 0, sizeof(sigchild_action));
-    sigchild_action.sa_handler = &child_handler;
-    sigaction(SIGCHLD, &sigchild_action, NULL);
-}
-
-// fork 出来的数据最好不要改父进程的数据。
-// 父进程继续运行，子进程运行完了要 exit.
+// 子进程先于父进程退出，父进程没有 wait 处理。
 void test_fork() {
-    for (int i = 0; i < CHILD_NUM; i++) {
-        pid_t pid = fork();
-        if (0 == pid) {
-            printf("child pid: %d\n", getpid());
-            // data.reset();
-            do_some_thing();
-            exit(0);
-        } else if (pid > 0) {
-            printf("parent pid: %d\n", getpid());
-        } else {
-            printf("failed!\n");
-        }
+    pid_t pid = fork();
+    if (0 == pid) {
+        printf("child pid: %d\n", getpid());
+        exit(0);
+    } else if (pid > 0) {
+        printf("parent pid: %d\n", getpid());
+    } else {
+        printf("fork failed!\n");
+        exit(1);
     }
 }
 
 int main() {
-    signal_set();
     test_fork();
-    do_some_thing();
-    wait(0);
+    sleep(10);
     return 0;
 }
