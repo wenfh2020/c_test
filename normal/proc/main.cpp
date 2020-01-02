0828924eb69dfb05b579dc2ae31a49dcc743acc1
wenfh2020 <wenfh2020@126.com>
2020-01-02 10:26:38 +0800

    测试进程 fork 进程

--- normal/proc/main.cpp
+++ normal/proc/main.cpp
@@ -30,8 +30,8 @@ class alloc_data {
         m_array = new int[m_len];
         if (m_array != NULL) {
             format_data(m_array, m_len);
-            printf("alloc array, this: %lx, ptr: %lx\n", (unsigned long)this,
-                   (unsigned long)&m_array);
+            printf("alloc, data ptr: %#lx, array ptr: %#lx\n",
+                   (unsigned long)this, (unsigned long)&m_array);
         } else {
             printf("alloc data fail, pid: %d\n", getpid());
         }
@@ -45,8 +45,8 @@ class alloc_data {
     }
 
     void reset() {
-        printf("reset array, this: %#lx, ptr: %#lx\n", (unsigned long)this,
-               (unsigned long)&m_array);
+        printf("reset data, data ptr: %#lx, array ptr: %#lx\n",
+               (unsigned long)this, (unsigned long)&m_array);
         format_data(m_array, m_len);
     }
     int get_data_len() { return m_len; }
@@ -102,23 +102,25 @@ int main() {
     signal_set();
     pid_t pid = fork();
     if (0 == pid) {
-        printf("child pid: %d\n", getpid());
+        printf("child pid: %d, data ptr: %#lx\n", getpid(),
+               (unsigned long)&g_data);
         sleep(10);  // update data before
+        printf("child pid: %d, reset data:\n", getpid());
         g_data.reset();
         sleep(10);  // update data later
         exit(0);
     } else if (pid > 0) {
-        printf("parent pid: %d\n", getpid());
+        printf("parent pid: %d, data ptr: %#lx\n", getpid(),
+               (unsigned long)&g_data);
         // sleep(10);
         // sleep() makes the calling process sleep until seconds seconds have
         // elapsed or a signal arrives which is not ignored.
     } else {
-        // fail
-        printf("fail\n");
+        printf("fork fail\n");
         exit(1);
     }
 
     do_some_thing();
-    printf("main end2, pid: %d\n", getpid());
+    printf("parent end, pid: %d\n", getpid());
     return 0;
 }
