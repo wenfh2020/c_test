12803d64a2ac43daac00eb25ae38024f96448029
wenfh2020 <wenfh2020@126.com>
2020-02-07 02:46:46 +0800

    对齐操作

--- normal/inherit/main.cpp
+++ normal/inherit/main.cpp
@@ -1,47 +1,104 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-const int g_array_len = 612;
-using namespace std;
+// #define DEBUG1
 
-void traversal(int len) {
-    vector<int> v;
-    for (int i = 0; i < len; i++) {
-        v.push_back(i);
-        printf("data: %d, size: %lu, capc: %lu\n", v[i], v.size(), v.capacity());
-    }
+typedef unsigned char u_char;
+
+#ifdef DEBUG1
+
+#define ALIGNMENT_4 4
+#define ALIGNMENT_8 8
+#define ALIGNMENT_13 13
+#define ALIGNMENT_16 16
+#define ngx_align_ptr(p, a) \
+    (u_char *)(((uintptr_t)(p) + ((uintptr_t)a - 1)) & ~((uintptr_t)a - 1))
+
+int main(int argc, char *argv[]) {
+    u_char *p = (u_char *)malloc(1024);
+    u_char *p1 = p + 2;
+    u_char *p2 = ngx_align_ptr(p1, ALIGNMENT_4);
+    u_char *p3 = p2 + 3;
+    u_char *p4 = ngx_align_ptr(p3, ALIGNMENT_8);
+    u_char *p5 = ngx_align_ptr(p + 11, ALIGNMENT_16);
+    printf("p:  %p\np1: %p\np2: %p\np3: %p\np4: %p\np5: %p\n", p, p1, p2, p3,
+           p4, p5);
+    printf("p2: %lu, p4: %lu, p5: %lu\n", (uintptr_t)p2 % ALIGNMENT_4,
+           (uintptr_t)p4 % ALIGNMENT_8, (uintptr_t)p5 % ALIGNMENT_16);
+    free(p);
 }
 
-void reserve(int len) {
-    vector<int> v;
-    v.reserve(len);
-    for (int i = 0; i < len; i++) {
-        v.push_back(i);
-        printf("data: %d, size: %lu, capc: %lu\n", v[i], v.size(), v.capacity());
+#else
+
+int sdsll2str(char* s, long long value) {
+    char *p, aux;
+    unsigned long long v;
+    size_t l;
+
+    /* Generate the string representation, this method produces
+     * an reversed string. */
+    v = (value < 0) ? -value : value;
+    p = s;
+    do {
+        *p++ = '0' + (v % 2);  // 2
+        v /= 2;                // 2
+    } while (v);
+    if (value < 0) *p++ = '-';
+
+    /* Compute length and add null term. */
+    l = p - s;
+    *p = '\0';
+
+    /* Reverse the string. */
+    p--;
+    while (s < p) {
+        aux = *s;
+        *s = *p;
+        *p = aux;
+        s++;
+        p--;
     }
+    return l;
 }
 
-void resize(int len) {
-    vector<int> v;
-    v.reserve(len);
-    printf("vector size: %lu, capc: %lu\n", v.size(), v.capacity());
-    v.resize(len+1, 5);
-    printf("vector size: %lu, capc: %lu\n", v.size(), v.capacity());
-    printf("v[%d] = %d\n", len+1, v[len+1]);
-    
+#define ngx_align_ptr(p, a) \
+    (u_char *)(((uintptr_t)(p) + ((uintptr_t)a - 1)) & ~((uintptr_t)a - 1))
+
+char* i2bin(unsigned long long v, char* buf, int len) {
+    if (0 == v) {
+        memcpy(buf, "0", 2);
+        return buf;
+    }
+
+    char* dst = buf + len - 1;
+    *dst = '\0';
+
+    while (v) {
+        if (dst - buf <= 0) return NULL;
+        *--dst = (v & 1) + '0';
+        v = v >> 1;
+    }
+    memcpy(buf, dst, buf + len - dst);
+    return buf;
 }
 
 int main() {
-    // 可以通过遍历数据，观察 vector 内部的内存分配情况。
-    // traversal(g_array_len);
+    int i;
+    uintptr_t l;
+    char* p;
+    char test[128];
+    int z[] = {1, 2, 4, 8, 16, 32, 64};
+    int len = sizeof(z) / sizeof(int);
 
-    // 预分配容器容量，观察容器内部的内存分配情况。
-    // reserve(g_array_len);
+    for (i = 0; i < len; i++) {
+        l = ~((uintptr_t)z[i] - 1);
+        p = i2bin(l, test, 128);
+        printf("a: %2d,  d: %s\n", z[i], p);
+        test[0] = '\0';
+    }
 
-    // 预分配容器容量，目标数据超出容量，观察容器内部的内存分配情况。
-    // reserve(g_array_len + 1);
-
-    // 
-    resize(g_array_len);
     return 0;
 }
+
+#endif
