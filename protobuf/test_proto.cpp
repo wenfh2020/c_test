5a1d397e5ffbf78016f7a949ad1e2b97b418da6f
wenfh2020 <wenfh2020@126.com>
2020-07-08 15:32:53 +0800

    test protobuf

---
+++ protobuf/test_proto.cpp
@@ -0,0 +1,28 @@
+
+/* wenfh2020 / 2020-07-08
+ * g++ -std='c++11' msg.pb.cc test_proto.cpp  -lprotobuf -o tproto && ./tproto 
+ */
+
+#include <iostream>
+#include <string>
+
+#include "msg.pb.h"
+
+int main(int argc, char** argv) {
+    MsgHead head;
+    head.set_cmd(1);
+    head.set_len(11);
+    head.set_seq(12);
+    std::cout << head.ByteSizeLong() << std::endl;
+
+    MsgBody body;
+    body.set_data("hello");
+    MsgBody_Request* req = body.mutable_req_target();
+    MsgBody_Response* rsp = body.mutable_rsp_result();
+    req->set_route_id(1);
+    req->set_route("123");
+    rsp->set_code(111);
+
+    std::cout << body.SerializePartialAsString() << std::endl;
+    return 0;
+}
