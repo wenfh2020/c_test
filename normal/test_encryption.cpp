3aa92df8f48d917fcb8ed5945882a0730caf8f5e
wenfh2020 <wenfh2020@126.com>
2020-07-09 12:00:04 +0800

    test cryptopp

---
+++ normal/test_encryption.cpp
@@ -0,0 +1,62 @@
+/*
+ * brew install cryptopp / yum install cryptopp
+ * g++ -std='c++11' test_encryption.cpp -lcryptopp -o test_encryption && ./test_encryption  
+ */
+
+#include <cryptopp/gzip.h>
+
+#include <iostream>
+
+bool gzip(const std::string& src, std::string& dst) {
+    try {
+        CryptoPP::Gzip zip;
+        zip.Put((CryptoPP::byte*)src.c_str(), src.size());
+        zip.MessageEnd();
+
+        CryptoPP::word64 avail = zip.MaxRetrievable();
+        if (avail) {
+            dst.resize(avail);
+            zip.Get((CryptoPP::byte*)&dst[0], dst.size());
+        }
+    } catch (CryptoPP::InvalidDataFormat& e) {
+        std::cout << e.GetWhat() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool ungzip(const std::string& src, std::string& dst) {
+    try {
+        CryptoPP::Gunzip zip;
+        zip.Put((CryptoPP::byte*)src.c_str(), src.size());
+        zip.MessageEnd();
+        CryptoPP::word64 avail = zip.MaxRetrievable();
+        if (avail) {
+            dst.resize(avail);
+            zip.Get((CryptoPP::byte*)&dst[0], dst.size());
+        }
+    } catch (CryptoPP::InvalidDataFormat& e) {
+        std::cout << e.GetWhat() << std::endl;
+        return false;
+    }
+    return (true);
+}
+
+int main(int argc, char** argv) {
+    // gzip
+    std::string src("hello world!"), dst;
+    if (gzip(src, dst)) {
+        std::cout << "gzip: " << dst << std::endl;
+    } else {
+        std::cout << "gzip failed!" << std::endl;
+    }
+
+    // ungizp
+    if (ungzip(dst, src)) {
+        std::cout << "ungzip: " << src << std::endl;
+    } else {
+        std::cout << "ungzip failed!" << std::endl;
+    }
+
+    return 0;
+}
