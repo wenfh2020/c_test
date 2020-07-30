f5cedab5245e97dbe542716c94b3bd4bc8575c66
wenfh2020 <wenfh2020@126.com>
2020-07-30 11:13:18 +0800

    yum install jemalloc-devel && yum install jemalloc

--- jemalloc/test_jemalloc.cpp
+++ jemalloc/test_jemalloc.cpp
@@ -1,22 +1,21 @@
-/*
- * wenfh2020.com / 2020-07-30
- * g++ -std='c++11' -g test_jemalloc.cpp  -o tjemalloc && ./tjemalloc
- * g++ -std='c++11' -g test_jemalloc.cpp  -o tjemalloc -DUSE_JEMALLOC -ljemalloc && ./tjemalloc
+/*                                                                                                 
+ * wenfh2020.com / 2020-07-30                                                                      
+ * g++ -std='c++11' -g test_jemalloc.cpp  -o tjemalloc && ./tjemalloc                              
+ * g++ -std='c++11' -g test_jemalloc.cpp  -o tjemalloc -DUSE_JEMALLOC -ljemalloc && ./tjemalloc    
 */
 
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 
 #include <iostream>
-#include <list>
 
 #ifdef USE_JEMALLOC
 #include <jemalloc/jemalloc.h>
 #endif
 
-#define MALLOC_CNT 100000
+#define MALLOC_CNT 10000000
 
-// 当前时间，精度毫秒
 long long mstime() {
     long long mst;
     struct timeval tv;
@@ -26,22 +25,15 @@ long long mstime() {
     return mst;
 }
 
-int main(int argc, char** argv) {
+int main() {
     srand((unsigned)time(NULL));
-    std::list<char*> memory;
     long long begin = mstime();
-
     for (int i = 0; i < MALLOC_CNT; i++) {
-        int size = 1024 + rand() % 1024;
+        int size = 1024 * 4 + rand() % 1024;
         char* p = (char*)malloc(size);
         memset(p, rand() % 128, size);
-        memory.push_back(p);
+        free(p);
     }
-    for (const auto& it : memory) {
-        free(it);
-    }
-    memory.clear();
-
     long long end = mstime();
 
     std::cout << "begin: " << begin << std::endl
