6db49f40900bab4a901cc58a9b732599b47c5b90
wenfh2020 <wenfh2020@126.com>
2020-10-16 13:02:45 +0800

    update split string.

--- normal/test_split_strings.cpp
+++ normal/test_split_strings.cpp
@@ -1,53 +1,51 @@
-/* g++ -std='c++11' test_split_strings.cpp -o test_split_strings && ./test_split_strings */
+/* refer: http://www.martinbroadhurst.com/how-to-split-a-string-in-c.html
+ * script: g++ -std='c++11' test_split_strings.cpp -o split && ./split */
+
 #include <iostream>
-#include <sstream>
+#include <string>
 #include <vector>
 
-void string_splits(const char* in, const char* sep, std::vector<std::string>& out, bool no_blank = true) {
-    if (in == nullptr || sep == nullptr || strlen(sep) > strlen(in)) {
-        return;
-    }
-
-    std::size_t pos;
-    std::string str(in);
-
-    while (1) {
-        pos = str.find(sep);
-        if (pos != std::string::npos) {
-            if (pos != 0) {
-                out.push_back(str.substr(0, pos));
-            }
-            if (str.length() == pos + 1) {
-                break;
-            }
-            str = str.substr(pos + 1);
-        } else {
-            out.push_back(str);
+void split(const std::string& in, std::vector<std::string>& out, const std::string& seq = " ", bool trim_blank = true) {
+    std::size_t cur = 0, pre = 0;
+    while ((pre = in.find_first_not_of(seq, cur)) != std::string::npos) {
+        cur = in.find(seq, pre);
+        if (cur == std::string::npos) {
+            cur = in.length();
+            out.push_back(in.substr(pre, cur - pre));
             break;
         }
+        out.push_back(in.substr(pre, cur - pre));
     }
 
-    /* trim blank. */
-    if (no_blank) {
-        auto it = out.begin();
-        while (it != out.end()) {
-            it->erase(0, it->find_first_not_of(" "));
-            it->erase(it->find_last_not_of(" ") + 1);
-            if (!it->empty()) {
-                it++;
-            } else {
-                it = out.erase(it);
-            }
+    if (trim_blank && seq != " ") {
+        for (auto& s : out) {
+            s.erase(0, s.find_first_not_of(" "));
+            s.erase(s.find_last_not_of(" ") + 1);
         }
     }
 }
 
 int main() {
-    std::vector<std::string> items;
-    const char* test = "   ,127.0.0.1:6379, 127.0.0.1:6378, 127.0.0.1:6377, ";
-    string_splits(test, ",", items);
-    for (auto& s : items) {
-        std::cout << s << std::endl;
+    std::vector<std::string> vec;
+
+    const char* seq = ",";
+    const char* s = ",127.0.0.1:6379,  127.0.0.1:6378,  127.0.0.1:6377";
+    // const char* seq = " ";
+    // const char* s = "127.0.0.1:6379    127.0.0.1:63178   127.0.0.1:65300   1";
+
+    split(s, vec, seq, false);
+    for (auto& s : vec) {
+        std::cout << "len: " << s.length() << " "
+                  << "data: " << s << std::endl;
+    }
+
+    std::cout << "----" << std::endl;
+
+    vec.clear();
+    split(s, vec, seq, true);
+    for (auto& s : vec) {
+        std::cout << "len: " << s.length() << " "
+                  << "data: " << s << std::endl;
     }
     return 0;
 }
